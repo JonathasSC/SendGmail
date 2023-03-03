1ffdc8a96c5a228c39b77cb5474c4992bae77cf8
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2023-03-03 15:15:40 -0300

    primeiros passos do projeto

--- sendgmail/emails/views.py
+++ sendgmail/emails/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.contrib import messages
+import datetime
 
 # Create your views here.
+
+def emailList(request):
+    return render(request, 'emails/list.html')
+
